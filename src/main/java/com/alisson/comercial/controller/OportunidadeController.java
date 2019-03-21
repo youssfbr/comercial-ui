1c15bb7abfbb5608271fc01bdf0abe828a1bb191
Alisson Youssf <youssfbr@gmail.com>
2019-03-21 16:47:39 -0300

    Listando por id - p1

--- src/main/java/com/alisson/comercial/controller/OportunidadeController.java
+++ src/main/java/com/alisson/comercial/controller/OportunidadeController.java
@@ -1,9 +1,11 @@
 package com.alisson.comercial.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +22,10 @@ public class OportunidadeController {
 	@GetMapping
 	public List<Oportunidade> listar() {
 		return oportunidades.findAll();
+	}
+	
+	@GetMapping("/{id}")
+	public Optional<Oportunidade> buscar(@PathVariable Long id) {
+		return oportunidades.findById(id);
 	}
 }

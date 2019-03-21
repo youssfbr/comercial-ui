b28522f5a6472eea924979b3e94f7122370a48be
Alisson Youssf <youssfbr@gmail.com>
2019-03-21 14:30:25 -0300

    Criando repositório OportunidadeRepository

---
+++ src/main/java/com/alisson/comercial/repository/OportunidadeRepository.java
@@ -0,0 +1,9 @@
+package com.alisson.comercial.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.alisson.comercial.model.Oportunidade;
+
+public interface OportunidadeRepository extends JpaRepository<Oportunidade, Long> {
+
+}

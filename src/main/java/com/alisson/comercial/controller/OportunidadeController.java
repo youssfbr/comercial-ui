1351ab1cdce65c261e7ced0aa4eea3feca84faba
Alisson Youssf <youssfbr@gmail.com>
2019-03-21 09:49:01 -0300

    Testando Model Oportunidade

--- src/main/java/com/alisson/comercial/controller/OportunidadeController.java
+++ src/main/java/com/alisson/comercial/controller/OportunidadeController.java
@@ -1,15 +1,27 @@
 package com.alisson.comercial.controller;
 
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.alisson.comercial.model.Oportunidade;
 
 @RestController
 @RequestMapping("/oportunidades")
 public class OportunidadeController {
 
 	@GetMapping
-	public String listar() {
-		return "Hello World";
+	public List<Oportunidade> listar() {
+		Oportunidade oportunidade = new Oportunidade();
+		oportunidade.setId(1L);
+		oportunidade.setNomeProspecto("Link Informática");
+		oportunidade.setDescrição("Desenvolvimento de ERP's com Spring MVC");
+		oportunidade.setValor(new BigDecimal(500000));
+		
+		return Arrays.asList(oportunidade);
 	}
 }

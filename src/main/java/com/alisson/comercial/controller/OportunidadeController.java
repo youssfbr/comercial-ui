aade6405244e70fde2e1d81922f4e644a0e1bb57
Alisson Youssf <youssfbr@gmail.com>
2019-03-21 09:19:26 -0300

    OportunidadeController REST criado e testado

---
+++ src/main/java/com/alisson/comercial/controller/OportunidadeController.java
@@ -0,0 +1,15 @@
+package com.alisson.comercial.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/oportunidades")
+public class OportunidadeController {
+
+	@GetMapping
+	public String listar() {
+		return "Hello World";
+	}
+}
